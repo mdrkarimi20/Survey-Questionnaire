e04e4bdf3d1e6daa9187ace630f4ef3bd502032b
mdrkarimi20 <mdrkarimi20@gmail.com>
2020-08-09 01:45:51 +0430

    edit mobile mode & desktop survey page

--- src/Views/Start/Start.jsx
+++ src/Views/Start/Start.jsx
@@ -3,21 +3,29 @@ import "./Start.scss";
 import { Link } from "react-router-dom";
 import { Button } from 'reactstrap';
 import { CSSTransition } from "react-transition-group";
-import Logo from "../../component/Logo/Logo";
 import Footer from "../../component/Footer/Footer";
 
-function Start({data}) {
+function Start({ data }) {
 
-        return (
+    return (
         <CSSTransition appear in classNames="page-transition" timeout={500}>
             <div className="Start">
                 <div className="Start-title">
-                    <h1 className="Start-title-text">فرم نظرسنجی و ارتقا سطح خدمت رسانی</h1>
-                    <Logo logo_url={data.logo_url} />
+                    <h1 className="Start-title-text pb-3">فرم نظرسنجی و ارتقا سطح خدمت رسانی</h1>
+                    <div className="logo">
+                        <img src={data.logo_url} alt={"astro-logo"} height="100%" width="25%" />
+                        <h2 className="astro pr-3">آسترو</h2>
+                    </div>
                     <Link to={`/question/${data.questions[0].id}`}>
                         <Button className="button-start">شروع</Button>
                     </Link>
-                    <Footer />
+                    <div className="footer">
+                        <div class="footer-top">
+                            <div className="footer-top-logo">Soally</div>
+                            <span>قدرت گرفته از</span>
+                        </div>
+                        <span className="footer-text">آنلاین CRM پلتفرم طراحی فرم های</span>
+                    </div>
                 </div>
             </div>
         </CSSTransition>
--- src/component/Logo/Logo.jsx
+++ src/component/Logo/Logo.jsx
@@ -5,7 +5,7 @@ function Logo({ logo_url }) {
     return (
         <div className="logo">
             <img src={logo_url} alt={"astro-logo"} height="100%" width="25%"/>
-            <h2 className="astro">آسترو</h2>
+            <h2 className="astro pr-3">آسترو</h2>
         </div>
     )
 }
